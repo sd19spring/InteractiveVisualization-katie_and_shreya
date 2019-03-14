a2f9ea28e84695260278f0ffa438716db9aa84ac
Katie <katief650@gmail.com>
2019-03-13 21:30:18 -0400

    Add c to clear, q to quit, and numbers to change color scheme

--- Drawing_Circles_Game.py
+++ Drawing_Circles_Game.py
@@ -52,13 +52,18 @@ done = False
 clock = pygame.time.Clock()
 
 
-#List of all the colors that does not currently do anything: possible_ball_colors = [RED, REDORANGE, ORANGE, YELLOWORANGE, YELLOW, YELLOWGREEN, GREEN, DARKGREEN,BLUEGREEN, CYAN, SKYBLUE, BLUE, INDIGO, LIGHTPURPLE, PURPLE, DARKPURPLE, MAGENTA, PINK, BLACK, DARKGREY, GREY, LIGHTGREY, WHITE, BROWN, DARKBROWN]
+# All color lists:
+all_colors = [RED, REDORANGE, ORANGE, YELLOWORANGE, YELLOW, YELLOWGREEN, GREEN, DARKGREEN,BLUEGREEN, CYAN, SKYBLUE, BLUE, INDIGO, LIGHTPURPLE, PURPLE, DARKPURPLE, MAGENTA, PINK, BLACK, DARKGREY, GREY, LIGHTGREY, WHITE, BROWN, DARKBROWN]
+ocean_colors = [DARKGREEN,BLUEGREEN,CYAN,SKYBLUE,BLUE,INDIGO,NIGHTSKY,DARKPURPLE,DARKGREY]
+pastel_colors = [LIGHTGREY,LIGHTPURPLE,PINK,WHITE,SKYBLUE]
+warm_colors = [RED,REDORANGE,ORANGE,YELLOWORANGE,YELLOW,MAGENTA,PINK]
+
 possible_speeds = [-3, -2, -1, 1, 2, 3]
 
 
 #Defines the class of circle(some charactaristics of the circle and everything it does)
 class Circle():
-    def __init__(self, x_location, y_location):
+    def __init__(self, x_location, y_location, color_list):
         self.x_location=x_location
         self.y_location=y_location
         #you can change the speeds here by making them equal to something else
@@ -66,7 +71,8 @@ class Circle():
         self.y_speed = random.choice(possible_speeds)
         #you can change the size here
         self.size = random.randint(7, 15)
-        self.color = (random.randint(0, 255), random.randint(0, 255),random.randint(0, 255))
+        # self.color = (random.randint(0, 255), random.randint(0, 255),random.randint(0, 255))
+        self.color = color_list[random.randint(0,len(color_list)-1)]
 
     #Defines the function move that draws the circles and makes them move offscreen
     def Move(self, screen):
@@ -104,12 +110,13 @@ class Circle():
         self.y_speed = self.y_speed/2
 
 
-
+print("Use Number keys to change colors, use c to clear and q to quit")
 
 #Creates ball list that the circles can be added to later
 ball_list = []
 
 drawing = False
+color_list = all_colors
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop
@@ -120,17 +127,31 @@ while not done:
             drawing = True
         elif event.type == pygame.MOUSEBUTTONUP:
             drawing = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_c:
+                ball_list.clear()
+            elif event.key == pygame.K_1:
+                color_list = all_colors
+            elif event.key == pygame.K_2:
+                color_list = ocean_colors
+            elif event.key == pygame.K_3:
+                color_list = pastel_colors
+            elif event.key == pygame.K_4:
+                color_list = warm_colors
+            elif event.key == pygame.K_q:
+                done = True
 
-    # drawing = True
+
     if drawing:
         pos=pygame.mouse.get_pos()
         x=pos[0]
         y=pos[1]
-        ball_list.append(Circle(x, y))
+        ball_list.append(Circle(x, y, color_list))
+
 
 
     #Background color
-    screen.fill(NIGHTSKY)
+    screen.fill(WHITE)
 
     #tells all balls in the list to fall
     for Ball in ball_list:
